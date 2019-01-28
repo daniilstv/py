3937478cf249fa85a6b9cc277f42d64e6b36a0e9
daniilstv <daniilstv@gmail.com>
2019-01-28 15:48:15 +0300

    b2

--- roll.py
+++ roll.py
@@ -14,36 +14,38 @@ def circle(r,col):
     turtle.begin_fill()
     turtle.circle(r)
     turtle.end_fill()
-    
-turtle.speed(40)
 
-gotoxy(0,0)
-turtle.circle(80)
-
-gotoxy(0,160)
-circle(5,'red')
-
-phi = 360 / 7
-r = 50
-
-
-
-for i in range(0,7):
-    phi_rad = phi * i * math.pi / 180.0
-    gotoxy(math.sin(phi_rad)*r, math.cos(phi_rad)*r + 60) 
-    circle(21,'brown')
-    circle(21,'white')
-
-
-gotoxy(math.sin(phi_rad)*r, math.cos(phi_rad)*r + 60) 
-
-circle(21,'brown')
-        
-answer = ''
+answer = 'y'    
 while answer != 'n':
 
-    answer = turtle.textinput("Повторитиь ?", "y/n")
+    answer = turtle.textinput("Сыграем ?", "y/n")
     if answer == 'y':
-        pass
+        
+        turtle.speed(40)
+
+        gotoxy(0,0)
+        turtle.circle(80)
+
+        gotoxy(0,160)
+        circle(5,'red')
+
+        phi = 360 / 7
+        r = 50
+
+
+
+        for i in range(0,7):
+            phi_rad = phi * i * math.pi / 180.0
+            gotoxy(math.sin(phi_rad)*r, math.cos(phi_rad)*r + 60) 
+            circle(21,'brown')
+            circle(21,'white')
+
+
+        gotoxy(math.sin(phi_rad)*r, math.cos(phi_rad)*r + 60) 
+
+        circle(21,'brown')
+        
+
     else:
         pass
+
