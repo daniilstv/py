d770b9d0dd3eea09e65447ee472900635f891f1a
daniilstv <daniilstv@gmail.com>
2019-01-28 15:33:19 +0300

    baraban

--- roll.py
+++ roll.py
@@ -1,27 +1,49 @@
 import turtle
+#  t = turtle.Turtle()
+import random
+import math
 
 
-turtle.circle(20)
+def gotoxy(x,y):
+    turtle.penup()
+    turtle.goto(x,y)
+    turtle.pendown()
 
+def circle(r,col):
+    turtle.fillcolor(col)
+    turtle.begin_fill()
+    turtle.circle(r)
+    turtle.end_fill()
+    
+turtle.speed(40)
+
+gotoxy(0,0)
+turtle.circle(80)
+
+gotoxy(0,160)
+circle(5,'red')
+
+phi = 360 / 7
+r = 50
+
+
+
+for i in range(0,7):
+    phi_rad = phi * i * math.pi / 180.0
+    gotoxy(math.sin(phi_rad)*r, math.cos(phi_rad)*r + 60) 
+    circle(21,'brown')
+    circle(21,'white')
+
+
+gotoxy(math.sin(phi_rad)*r, math.cos(phi_rad)*r + 60) 
+
+circle(21,'brown')
+        
 answer = ''
-#while answer != 'N':
-#    answer = turtle.textinput("Нарисовать ?", "Y/N")
+while answer != 'n':
 
-r = 200
-x = 0
-y = 0 - r / 2
-
-        gotoxy(x,y)
-        turtle.fillcolor('red')
-
-        turtle.circle(r)
-        turtle.end_fill()
-
-        turtle.color('white')
-
-        phi = math.pi * 2 / 5
-        for i in range(1,6):
-            xi = math.sin(math.pi + phi*i*2)*r-x
-            yi = math.cos(math.pi + phi*i*2)*r-y
-
-            turtle.goto(xi, yi)
+    answer = turtle.textinput("Повторитиь ?", "y/n")
+    if answer == 'y':
+        pass
+    else:
+        pass
